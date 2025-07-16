14dc31b07ee367d6b0408dbd13d1c101fd902628
Jorge Gutierrez <jomiguma_10@hotmail.com>
2025-07-16 00:44:15 -0700

    finally added the font for the app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,25 @@
+import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
 
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    'PressStart2P': require('../assets/fonts/PressStart2P-Regular.ttf'),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
