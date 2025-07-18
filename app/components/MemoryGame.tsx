131e627d3c60a6bcaf80f76e4c5e29ea975d4914
Jorge Gutierrez <jomiguma_10@hotmail.com>
2025-07-17 17:20:47 -0700

    changed code

--- app/components/MemoryGame.tsx
+++ app/components/MemoryGame.tsx
@@ -292,6 +292,14 @@ const styles = StyleSheet.create({
     position: 'absolute',
     width: '100%',
     height: '100%',
+    ...Platform.select({
+          ios: {
+            shadowColor: '#000',
+            shadowOffset: { width: 0, height: 2 },
+            shadowOpacity: 0.25,
+            shadowRadius: 3.84,
+          }
+        }),
   },
   cardBack: {
     backgroundColor: '#fff9b0',
